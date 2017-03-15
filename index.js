909d65860360ff7bb4301e159e94cb5f317239fd
theConscience <vinton.mail@gmail.com>
2017-03-15 18:48:54 +0300

    commit2
    change regulars & content to working propertly in theconscience

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = function ( option ) {
     'use strict';
 
     var CONST_PATTERN = '<\\!--\\s*inject-style\\s*(.*?)\\s*-->';
-    var CSS_LINK_PATTERN = '<link .*href="([^\.]\.css)"[^>]*>';
-    var JS_SCRIPT_PATTERN = '<script .*src="([^\.]\.js)"[^>]*>';
+    var CSS_LINK_PATTERN = '<link\\s*(.*?)\\s*>';//'<link href="([^\\.]\\.css)"[^>]*>';
+    var JS_SCRIPT_PATTERN = '<script\\s*(.*?)\\s*></script>';
 
     var self = null;
 
@@ -51,6 +51,7 @@ module.exports = function ( option ) {
     if (!option.path) {
         option.path = '';
     }
+    console.log('option.path = ' + option.path);
 
     if ( option.static_url_path ) {
         try {
@@ -85,6 +86,12 @@ module.exports = function ( option ) {
         option.django_ext_variable = '{{ STATIC_EXT }}';
     }
 
+    console.log('option.css_match_pattern = ' + option.css_match_pattern);
+    console.log('option.js_match_pattern = ' + option.js_match_pattern);
+    console.log('option.static_url_path = ' + option.static_url_path);
+    console.log('option.django_static_variable = ' + option.django_static_variable);
+    console.log('option.django_ext_variable = ' + option.django_ext_variable);
+
     function throwError( msg ) {
         self.emit( 'error',
             new gutil.PluginError( 'gulp-style-inject', msg ) );
@@ -95,15 +102,25 @@ module.exports = function ( option ) {
     }
 
     function transformLinkResponse( contents ) {
-        return '\n' + contents + '\n';
+        return '' + contents + '\n';
     }
 
     function transformScriptResponse( contents ) {
-        return '\n' + contents + '\n';
+        return '' + contents + '\n';
     }
 
     function getAttributes( params ) {
         new gutil.log( 'getAttributes get such parameters:' + params );
+
+        if (params.indexOf(option.django_static_variable) !== -1) {
+            params = params.replace(option.django_static_variable, option.static_url_path);
+            new gutil.log('django_static_variable replaced, params is: ' + params);
+        }
+        if (params.indexOf(option.django_ext_variable) !== -1) {
+            params = params.replace(option.django_ext_variable, '');
+            new gutil.log('django_ext_variable deleted, params is: ' + params);
+        }
+
         var result = {};
         var group = params.replace( /\s+/gi, ' ' )
             .split( ' ' );
@@ -161,46 +178,34 @@ module.exports = function ( option ) {
 
         // check if file.contents is a `Buffer`
         if ( file.isBuffer() ) {
+            new gutil.log('file is buffer!');
             var contents = String( file.contents );
 
+            new gutil.log('Files contents before regexp: ' + contents);
+
             contents = contents.replace( new RegExp( option.match_pattern, 'gi' ), function ( match, parameters ) {
-                new gutil.log('Files contents after regexp: ' + contents);
+                new gutil.log('Files contents after regexp match_pattern: ' + contents);
                 var attrs = getAttributes( parameters );
                 new gutil.log('option.path is: ' + option.path);
                 return getStyleFile( option.path + attrs.src );
             } );
 
             contents = contents.replace( new RegExp( option.css_match_pattern, 'gi' ), function ( match, parameters ) {
-                new gutil.log('Files contents after regexp: ' + contents);
+                new gutil.log('Files contents after regexp css_match_pattern!: ' + contents);
                 var attrs = getAttributes( parameters );
+                console.log('here!');
                 if (attrs.href !== 'undefined') {
                     new gutil.log('Get link, href value is: ' + attrs.href);
-                    if (attrs.href.indexOf(options.django_static_variable) !== -1) {
-                        attrs.href = attrs.href.replace(options.django_static_variable, options.static_url_path);
-                        new gutil.log('django_static_variable replaced, href value is: ' + attrs.href);
-                    }
-                    if (attrs.href.indexOf(options.django_ext_variable) !== -1) {
-                        attrs.href = attrs.href.replace(options.django_ext_variable, '');
-                        new gutil.log('django_ext_variable deleted, href value is: ' + attrs.href);
-                    }
                     new gutil.log('option.path is: ' + option.path);
                     return getCssFile( option.path + attrs.href );
                 }
             } );
 
             contents = contents.replace( new RegExp( option.js_match_pattern, 'gi' ), function ( match, parameters ) {
-                new gutil.log('Files contents after regexp: ' + contents);
+                new gutil.log('Files contents after regexp js_match_pattern: ' + contents);
                 var attrs = getAttributes( parameters );
                 if (attrs.src !== 'undefined') {
                     new gutil.log('Get script, src value is: ' + attrs.src);
-                    if (attrs.src.indexOf(options.django_static_variable) !== -1) {
-                        attrs.src = attrs.src.replace(options.django_static_variable, options.static_url_path);
-                        new gutil.log('django_static_variable replaced, src value is: ' + attrs.src);
-                    }
-                    if (attrs.src.indexOf(options.django_ext_variable) !== -1) {
-                        attrs.src = attrs.src.replace(options.django_ext_variable, '');
-                        new gutil.log('django_ext_variable deleted, src value is: ' + attrs.src);
-                    }
                     new gutil.log('option.path is: ' + option.path);
                     return getJsFile( option.path + attrs.src );
                 }
@@ -209,6 +214,8 @@ module.exports = function ( option ) {
             file.contents = new Buffer( contents );
             this.push( file );
             return callback();
+        } else {
+            console.log('file not is buffer!');
         }
 
         return callback();
