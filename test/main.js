4c78c29f48722c9ed5403c6d8c7f88f24cf6616a
theConscience <vinton.mail@gmail.com>
2017-03-18 17:28:31 +0300

    comment my awfull tests >:)

--- test/main.js
+++ test/main.js
@@ -12,109 +12,25 @@ delete require.cache[require.resolve('../')];
 var gutil = require('gulp-util'),
     styleInject = require('../');
 
-// describe('gulp-style-inject', function() {
-//
-//     it('should produce expected file via buffer', function(done) {
-//
-//         var srcFile = new gutil.File({
-//             path: 'test/fixtures/index.html',
-//             cwd: 'test/',
-//             base: 'test/fixtures',
-//             contents: fs.readFileSync('test/fixtures/index.html')
-//         });
-//
-//         var expectedFile = new gutil.File({
-//             path: 'test/expected/index.html',
-//             cwd: 'test/',
-//             base: 'test/expected',
-//             contents: fs.readFileSync('test/expected/index.html')
-//         });
-//
-//         var stream = styleInject();
-//
-//         stream.on('error', function(err) {
-//             should.exist(err);
-//             done(err);
-//         });
-//
-//         stream.on('data', function(newFile) {
-//
-//             should.exist(newFile);
-//             should.exist(newFile.contents);
-//
-//             String(newFile.contents)
-//                 .should.equal(String(expectedFile.contents));
-//             done();
-//         });
-//
-//         stream.write(srcFile);
-//         stream.end();
-//     });
-//
-//     it('should error on stream', function(done) {
-//
-//         var srcFile = new gutil.File({
-//             path: 'test/fixtures/index.html',
-//             cwd: 'test/',
-//             base: 'test/fixtures',
-//             contents: fs.createReadStream('test/fixtures/index.html')
-//         });
-//
-//         var stream = styleInject();
-//
-//         stream.on('error', function(err) {
-//             should.exist(err);
-//             done();
-//         });
-//
-//         stream.on('data', function(newFile) {
-//             newFile.contents.pipe(es.wait(function(err) {
-//                 done(err);
-//             }));
-//         });
-//
-//         stream.write(srcFile);
-//         stream.end();
-//     });
-//
-// });
+describe('gulp-style-inject', function() {
 
-
-describe('gulp-file-inject', function() {
-
-    it('should produce expected file bundles via buffer', function(done) {
+    it('should produce expected file via buffer', function(done) {
 
         var srcFile = new gutil.File({
-            path: 'test/fixtures/test2/test2.html',
+            path: 'test/fixtures/index.html',
             cwd: 'test/',
             base: 'test/fixtures',
-            contents: fs.readFileSync('test/fixtures/test2/test2.html')
+            contents: fs.readFileSync('test/fixtures/index.html')
         });
 
-        // var expectedFile1 = new gutil.File({
-        //     path: 'test/expected/test1/test1.libs.bundle.js',
-        //     cwd: 'test/',
-        //     base: 'test/expected/',
-        //     contents: fs.readFileSync('test/expected/test1/test1.libs.bundle.js')
-        // });
-        //
-        // var expectedFile2 = new gutil.File({
-        //     path: 'test/expected/test1/test1.plugins.bundle.js',
-        //     cwd: 'test/',
-        //     base: 'test/expected/',
-        //     contents: fs.readFileSync('test/expected/test1/test1.plugins.bundle.js')
-        // });
-        //
-        // var expectedFile3 = new gutil.File({
-        //     path: 'test/expected/test1/test1.plugins2.bundle.js',
-        //     cwd: 'test/',
-        //     base: 'test/expected/',
-        //     contents: fs.readFileSync('test/expected/test1/test1.plugins2.bundle.js')
-        // });
+        var expectedFile = new gutil.File({
+            path: 'test/expected/index.html',
+            cwd: 'test/',
+            base: 'test/expected',
+            contents: fs.readFileSync('test/expected/index.html')
+        });
 
-        // var expectedDirFilesA = fs.readdirSync('test/expected');
-
-        var stream = styleInject({static_url_path: 'test/fixtures/test2/'});
+        var stream = styleInject();
 
         stream.on('error', function(err) {
             should.exist(err);
@@ -123,14 +39,11 @@ describe('gulp-file-inject', function() {
 
         stream.on('data', function(newFile) {
 
-            console.log('\n**** NEW File in stream:', newFile);
-            // should.exist(newFile);
-            // should.exist(newFile.contents);
+            should.exist(newFile);
+            should.exist(newFile.contents);
 
-            // String(newFile.contents)
-            //     .should.equal(String(expectedFile3.contents));
-
-            // console.log('newFile.contents:', newFile.contents);
+            String(newFile.contents)
+                .should.equal(String(expectedFile.contents));
             done();
         });
 
@@ -147,7 +60,7 @@ describe('gulp-file-inject', function() {
             contents: fs.createReadStream('test/fixtures/index.html')
         });
 
-        var stream = styleInject({static_url_path: 'test/fixtures/'});
+        var stream = styleInject();
 
         stream.on('error', function(err) {
             should.exist(err);
@@ -165,3 +78,90 @@ describe('gulp-file-inject', function() {
     });
 
 });
+
+
+// describe('gulp-file-inject', function() {
+//
+//     it('should produce expected file bundles via buffer', function(done) {
+//
+//         var srcFile = new gutil.File({
+//             path: 'test/fixtures/test2/test2.html',
+//             cwd: 'test/',
+//             base: 'test/fixtures',
+//             contents: fs.readFileSync('test/fixtures/test2/test2.html')
+//         });
+//
+//         // var expectedFile1 = new gutil.File({
+//         //     path: 'test/expected/test1/test1.libs.bundle.js',
+//         //     cwd: 'test/',
+//         //     base: 'test/expected/',
+//         //     contents: fs.readFileSync('test/expected/test1/test1.libs.bundle.js')
+//         // });
+//         //
+//         // var expectedFile2 = new gutil.File({
+//         //     path: 'test/expected/test1/test1.plugins.bundle.js',
+//         //     cwd: 'test/',
+//         //     base: 'test/expected/',
+//         //     contents: fs.readFileSync('test/expected/test1/test1.plugins.bundle.js')
+//         // });
+//         //
+//         // var expectedFile3 = new gutil.File({
+//         //     path: 'test/expected/test1/test1.plugins2.bundle.js',
+//         //     cwd: 'test/',
+//         //     base: 'test/expected/',
+//         //     contents: fs.readFileSync('test/expected/test1/test1.plugins2.bundle.js')
+//         // });
+//
+//         // var expectedDirFilesA = fs.readdirSync('test/expected');
+//
+//         var stream = styleInject({static_url_path: 'test/fixtures/test2/'});
+//
+//         stream.on('error', function(err) {
+//             should.exist(err);
+//             done(err);
+//         });
+//
+//         stream.on('data', function(newFile) {
+//
+//             console.log('\n**** NEW File in stream:', newFile);
+//             // should.exist(newFile);
+//             // should.exist(newFile.contents);
+//
+//             // String(newFile.contents)
+//             //     .should.equal(String(expectedFile3.contents));
+//
+//             // console.log('newFile.contents:', newFile.contents);
+//             done();
+//         });
+//
+//         stream.write(srcFile);
+//         stream.end();
+//     });
+//
+//     it('should error on stream', function(done) {
+//
+//         var srcFile = new gutil.File({
+//             path: 'test/fixtures/index.html',
+//             cwd: 'test/',
+//             base: 'test/fixtures',
+//             contents: fs.createReadStream('test/fixtures/index.html')
+//         });
+//
+//         var stream = styleInject({static_url_path: 'test/fixtures/'});
+//
+//         stream.on('error', function(err) {
+//             should.exist(err);
+//             done();
+//         });
+//
+//         stream.on('data', function(newFile) {
+//             newFile.contents.pipe(es.wait(function(err) {
+//                 done(err);
+//             }));
+//         });
+//
+//         stream.write(srcFile);
+//         stream.end();
+//     });
+//
+// });
